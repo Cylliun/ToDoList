b9c4f991466af21d0d9e54495d307543508ac64c
Cylliun <japaguilherme55@gmail.com>
2024-08-21 15:05:35 -0400

    Criando Banco de Dados

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8702f2cec7439c81d0f2b76337539a984b96d543")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
