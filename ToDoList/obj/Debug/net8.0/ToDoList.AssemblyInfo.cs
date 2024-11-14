842f08f43588590de75f3c66dc65db0358cd9c2d
Cylliun <japaguilherme55@gmail.com>
2024-11-14 14:57:18 -0400

    arquivos docker criados

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3ddf1c7c829da715f6d39668aa2550f9ead5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f005228bd04d13054bbc54f740e43672201d907b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
